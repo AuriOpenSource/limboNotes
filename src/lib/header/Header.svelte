8130a9ba956ae89096ca8f38082ecd51a4ad9ef7
Roandol <roan.lopes10@hotmail.com>
2022-04-08 13:15:30 -0300

    list and note

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -7,7 +7,7 @@
 <header class="fixed border-b-2 border-primary py-1 w-full h-auto flex justify-between z-40 bg-base-100">
 	<div class="grid grid-cols-2">
 		<div class="border-b-2 border-green-500 w-full hover:bg-base-300 ml-2 flex place-items-center">
-			<a href="" class="mx-2">notes</a>
+			<a href="/notes" class="mx-2">notes</a>
 		</div>
 	</div>
 	<Search/>
