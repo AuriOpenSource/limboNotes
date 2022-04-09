947313bc94f89aae2dc970f5256fc34628863973
Roandol <roan.lopes10@hotmail.com>
2022-04-09 18:06:35 -0300

    pwa suports

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,8 @@
 const CACHE_NAME= 'limboCache';
 const urlsToCache = [
     '/',
-    '/notes'
+    '/notes',
+
 ]
 
 self.addEventListener('install', (evt) => {
@@ -23,3 +24,33 @@ self.addEventListener('fetch', (evt) => {
         })
     )
 })
+
+// importScripts(
+//     "https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js"
+//   );
+//   if (workbox) {
+//     workbox.setConfig({
+//       debug: false,
+//     });
+//     const wkb = {
+//       ...workbox.core,
+//       ...workbox.recipes,
+//     };
+//     const {
+//       clientsClaim,
+//       pageCache,
+//       staticResourceCache,
+//       googleFontsCache,
+//       imageCache,
+//     } = wkb;
+  
+//     imageCache();
+//     pageCache();
+//     googleFontsCache();
+//     staticResourceCache({
+//       cacheName: "limboCache",
+//     });
+//     clientsClaim();
+//   } else {
+//     console.error("Boo! Workbox failed to load");
+//   }
