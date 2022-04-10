c79e75f574334c81577083295bb88560700b2a34
Roandol <roan.lopes10@hotmail.com>
2022-04-10 12:06:39 -0300

    pwa suports

--- src/lib/header/components/category.svelte
+++ src/lib/header/components/category.svelte
@@ -3,5 +3,6 @@
 </script>
 
 <div class="grid grid-cols-2">
+    <a href="/" class="flex place-items-center px-2 border-b-2 border-secondary">Home</a>
     <a href="/notes" class="flex place-items-center px-2 border-b-2 border-secondary">notes</a>
 </div>
