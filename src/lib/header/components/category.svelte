55b79dfec47bbda9493df58e9505e109389cc8db
Roandol <roan.lopes10@hotmail.com>
2022-04-10 13:52:43 -0300

    pwa suports

--- src/lib/header/components/category.svelte
+++ src/lib/header/components/category.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     
+    const links = [
+        ['/', 'home', false],
+        ['/notes', 'notes', false]
+    ]
+    
+    onMount(() => {
+        links
+        .forEach((e, i) => {
+            if (links[i][0] == self.location.pathname) links[i][2] = true
+        })
+    })
+
+    function underline() {
+        console.log(this.textContent);
+
+        const {index} = this.dataset
+
+        links
+        .forEach((e , i) => {
+            links[i][2] = false
+        });
+        links[index][2] = true
+    }
 </script>
 
 <div class="grid grid-cols-2">
-    <a href="/" class="flex place-items-center px-2 border-b-2 border-secondary">Home</a>
-    <a href="/notes" class="flex place-items-center px-2 border-b-2 border-secondary">notes</a>
+    {#each links as [path, name, activate], id}
+    <a 
+        href={path}
+        data-index={id}
+        on:click={underline} 
+        class:border-b-2={activate} 
+        class="flex place-items-center px-2 border-secondary transition-all"
+    >
+        {name}
+    </a>
+    {/each}
 </div>
